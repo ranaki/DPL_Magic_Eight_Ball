7ca7a3d73f72cdff17f893b1524bd8edc9e8c829
rick baker <baker.rickw@gmail.com>
2017-01-29 00:00:39 -0700

    remove comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,5 @@
 // Magic Eight Ball 
 
-// function popQuestion(){
-//     var question = prompt("Type your question for Magic Eight Ball", "Am I ...");
-//     if (question != null) {
-//         document.getElementById("question-text").innerHTML =
-//         "You asked: " + question + "?";
-//     }
-// }
 var questionAnswerHist = [];
 
 var answers = ["Yup",
