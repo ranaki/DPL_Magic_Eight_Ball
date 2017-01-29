ebf70ec483022887a93e858f63df5eaeb88925f4
rick baker <baker.rickw@gmail.com>
2017-01-28 23:50:29 -0700

    work on bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,56 @@
 // Magic Eight Ball 
 
-function popQuestion(){
-    var question = prompt("Type your question for Magic Eight Ball", "Am I ...");
-    if (question != null) {
-        document.getElementById("question-text").innerHTML =
-        "You asked: " + question + "?";
-    }
+// function popQuestion(){
+//     var question = prompt("Type your question for Magic Eight Ball", "Am I ...");
+//     if (question != null) {
+//         document.getElementById("question-text").innerHTML =
+//         "You asked: " + question + "?";
+//     }
+// }
+var questionAnswerHist = [];
+
+var answers = ["Yup",
+               "Maybe",
+               "No way",
+               "Could be",
+               "Never",
+               "Absolutely",
+               "Ha"];
+
+var customAnswers = [];
+
+function getQuestion() {
+    var myQuestion = document.getElementById("ball-question-input").value;
     
+    checkQuestionForCustom(myQuestion);
+
+    answers = answers.concat(customAnswers);
+
+    var answer = answers[getRandomAnswerIndex()];
+
+    document.getElementById("answer-p").innerHTML = answer;
+
+    questionAnswerHist.push(myQuestion + "-" + answer);
+
+    document.getElementById("ball-question-input").value = '';
+}
+
+function getRandomAnswerIndex(){
+    var mergeAnswersArray = answers.concat(customAnswers);
+    var arrayMax = mergeAnswersArray.length;
+    var randomNum = Math.floor(Math.random() * arrayMax);
+    return randomNum;
+}
+
+function checkQuestionForCustom(currQuestion) {
+    // debugger;
+    if (currQuestion.includes("add_answer:")) {
+        customAnswers.push(currQuestion.split(":")[1]);
+    }
+    if (currQuestion.includes("clear_answers")) {
+        customAnswers = [];
+    }
+    if (currQuestion.includes("clear_history")) {
+        questionAnswerHist = [];
+    }
 }
