83a26098c7b23286652c9c5879e8ae117cd38144
rick baker <baker.rickw@gmail.com>
2017-01-29 19:29:58 -0700

    working on history

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,20 +15,25 @@ var customAnswers = [];
 function getQuestion() {
     var myQuestion = document.getElementById("ball-question-input").value;
     
+    if (myQuestion.trim() === '') 
+    {
+        alert("Please enter a Question...");
+        return;
+    }
+
     checkQuestionForCustom(myQuestion);
-
-    answers = answers.concat(customAnswers);
-
-    var answer = answers[getRandomAnswerIndex()];
+    // debugger;
+    var answersConcat = answers.concat(customAnswers);
+    var answer = answersConcat[getRandomAnswerIndex()];
 
     document.getElementById("answer-p").innerHTML = answer;
-
     questionAnswerHist.push(myQuestion + "-" + answer);
-
+    addToHistory();
     document.getElementById("ball-question-input").value = '';
 }
 
 function getRandomAnswerIndex(){
+    // debugger;
     var mergeAnswersArray = answers.concat(customAnswers);
     var arrayMax = mergeAnswersArray.length;
     var randomNum = Math.floor(Math.random() * arrayMax);
@@ -36,14 +41,33 @@ function getRandomAnswerIndex(){
 }
 
 function checkQuestionForCustom(currQuestion) {
-    // debugger;
+    var newAnswer = '';
     if (currQuestion.includes("add_answer:")) {
-        customAnswers.push(currQuestion.split(":")[1]);
+        newAnswer = currQuestion.split(":")[1];
+        if (!customAnswers.includes(newAnswer))
+        {
+            customAnswers.push(newAnswer);
+        }
+        return;
     }
     if (currQuestion.includes("clear_answers")) {
         customAnswers = [];
+        return;
     }
     if (currQuestion.includes("clear_history")) {
         questionAnswerHist = [];
+        return;
     }
+}
+function addToHistory(){
+    //for each item in history array make list
+    var histList = document.getElementById("history-ul");
+    histList.innerHTML = '';
+    var li;
+    questionAnswerHist.forEach(function (element) {
+        li = document.createElement("li");
+        li.innerHTML = element;
+        histList.appendChild(li);
+    })
+
 }
